186149cd37448ee9b376a7d2a7e8e4892ac9d078
20152842 <20152842@kookmin.ac.kr>
2023-03-02 16:45:53 +0900

    commit test/2023-03-02

--- Capstone/src/main/java/lastcoder/service/urlService.java
+++ Capstone/src/main/java/lastcoder/service/urlService.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigInteger;
 
 import org.apache.tomcat.util.codec.binary.Base64;
@@ -33,7 +34,7 @@ public class urlService {
 	}
 
 	public byte[] fileToByteArray(File file) {
-		String out = new String();
+//		String out = new String();
 		FileInputStream fis = null;
 		byte[] fileArray = null;
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -89,45 +90,63 @@ public class urlService {
 
 		int[][] imageArray = new int[128][128];
 		String binaryArray = info.getBinary_array();
-		
-		int j, k = 0;
-		int tmp = -1;
-		System.out.println(binaryArray.length());
 
-		for(int i = 0; 14 <= binaryArray.length() - i ; i += 14){
-			System.out.println("i = " + i);
+		int j, k = 0;
+
+		for (int i = 0; 14 <= binaryArray.length() - i; i += 14) {
 			j = Byte.parseByte(binaryArray.substring(i, i + 7), 2);
 			k = Byte.parseByte(binaryArray.substring(i + 7, i + 14), 2);
-			 
-			System.out.println(j +", " + k);
 			if (imageArray[j][k] < 255) {
 				imageArray[j][k] += 1;
 			}
-			tmp = i;
 		}
-		System.out.println(binaryArray.length() - tmp);
 
 		info.setImageArray(imageArray);
+		saveCSV(info);
 	}
 
-	
+	public void saveCSV(info info) {
+		File csv = new File("D:\\image.csv");
+		PrintWriter writer;
+		StringBuilder sb = new StringBuilder();
 
-	
+		int[][] imageArray = info.getImageArray();
 
-	public void pythonExec() {
+		try {
+			writer = new PrintWriter(csv);
+
+			for (int i = 0; i < imageArray.length; i++) {
+				for (int k = 0; k < imageArray[0].length; k++) {
+					if (i == imageArray.length - 1 & k == imageArray.length -1) {
+						sb.append(imageArray[i][k]);
+					} else {
+						sb.append(imageArray[i][k] + ", ");
+					}
+				}
+				sb.append('\n');
+			}
+			writer.write(sb.toString());
+			writer.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}
+	}
+
+	public void pythonExec(int[][] imageArray) {
 
 		PythonInterpreter interpreter = new PythonInterpreter();
 
 		interpreter.execfile("D:\\test.py");
-		interpreter.exec("print(testFunc(5,10))");
+//		interpreter.exec("print(testFunc(5,10))");
 
 		PyFunction pyFunction = interpreter.get("testFunc", PyFunction.class);
 
 		int a = 10;
 		int b = 20;
-
+//		PyObject array = Object( 	imageArray);
 		PyObject pyObject = pyFunction.__call__(new PyInteger(a), new PyInteger(b));
-		System.out.println(pyObject.toString());
+//		System.out.println(pyObject.toString());
 	}
 
 }
