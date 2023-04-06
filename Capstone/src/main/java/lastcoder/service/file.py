9da86eb6ba2b1c1404fbe39c31538c7daafd7ec3
yonghoya <dydgh0608@kookmin.ac.kr>
2023-04-06 09:58:44 +0900

    file.py modify

--- Capstone/src/main/java/lastcoder/service/file.py
+++ Capstone/src/main/java/lastcoder/service/file.py
@@ -1,9 +1,14 @@
+from PIL import image
 import sys
 
-def to_upper(input_str):
+def binary(input_str):
+    hex_list = input_str.split(" ")
+    result_binary = ""
+    for hex in hex_list:
+        result_binary += bin(int(hex, 16))[2:0].zfill(8)
     return input_str[0:5]
 
 if __name__ == '__main__':
     input_str = sys.argv[1]
-    output_str = to_upper(input_str)
+    output_str = binary(input_str)
     print(output_str)
