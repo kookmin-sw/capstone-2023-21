316e6ebfe446e6a110d73d4df20d3ed4a4f26810
yonghoya <dydgh0608@kookmin.ac.kr>
2023-04-12 12:27:57 +0900

    test 12 :27

--- Capstone/src/main/java/lastcoder/service/file.py
+++ Capstone/src/main/java/lastcoder/service/file.py
@@ -1,10 +1,6 @@
 import sys
 
 def binary(input_str):
-    hex_list = input_str.split(" ")
-    result_binary = ""
-    for hex in hex_list:
-        result_binary += bin(int(hex, 16))[2:0].zfill(8)
     return input_str[0:5]
 
 if __name__ == '__main__':
