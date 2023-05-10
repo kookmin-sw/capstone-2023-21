cf735af583d0e809a16edaa1601ac6946ca5ca65
yonghoya <dydgh0608@kookmin.ac.kr>
2023-05-10 14:23:28 +0900

    mainController code review

--- Capstone/src/main/java/lastcoder/controller/mainController.java
+++ Capstone/src/main/java/lastcoder/controller/mainController.java
@@ -8,21 +8,16 @@ package lastcoder.controller;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import lastcoder.model.info;
 import lastcoder.service.urlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import lastcoder.model.info;
-import lastcoder.service.urlService;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
@@ -33,8 +28,35 @@ public class mainController {
 	@Autowired
 	private urlService urlService;
 
+	// PE파일 분류 함수
+	public void checked_PEfile(String fileName, String upload_filePath, List PEfileList, MultipartFile inputFile){
 
-	//test
+		// PE파일 확장자들을 저장한 리스트
+		List<String> pefile_extensions = Arrays.asList("exe", "src", "dll", "ocx", "cpl", "drv", "sys", "vxd", "obj");
+		// 경로 split
+		String[] extension = fileName.split("\\.");
+
+		// 확장자 검사
+		if(pefile_extensions.contains(extension[extension.length-1])){
+
+			// 업로드할 경로에 파일 생성
+			File uploadFile = new File(upload_filePath + File.separator + fileName);
+			// True일 경우 리스트에 추가
+			PEfileList.add(uploadFile);
+
+			try{
+				// 입력 받은 파일을 지정한 경로(upload)에 저장
+				inputFile.transferTo(uploadFile);
+			}catch (IllegalStateException e){
+				e.printStackTrace();
+			}catch (IOException e){
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
 	@RequestMapping("/main")
 	@ResponseBody
 	public String hello() {
@@ -50,50 +72,38 @@ public class mainController {
 	
 	@PostMapping("/receive_URL")
 	@ResponseBody
-	public ModelAndView receiveURL(MultipartHttpServletRequest multipartFile, Model model) throws IOException {
+	public ModelAndView receiveURL(MultipartHttpServletRequest multipartFile) throws IOException {
 
+		// 입력받은 파일들을 저장한 리스트
 		List<MultipartFile> fileinputlist = multipartFile.getFiles("multipartFile");
-		List<File> PEfile = new ArrayList<File>();
-		String filePath = "C:\\Users\\82109\\Desktop\\real\\capstone-2023-21\\Capstone\\quarantine";
+		// PE 파일들을 저장하는 리스트
+		List<File> PEfileList = new ArrayList<File>();
+		// 업로드할 파일 경로
+		String upload_filePath = "C:\\Users\\82109\\Desktop\\real\\capstone-2023-21\\Capstone\\quarantine";
 
+		// 입력받은 파일들의 이름 추출 및 PE파일 분류
 		for(int i =0; i< fileinputlist.size(); i++){
 
-			String fileRealName = fileinputlist.get(i).getOriginalFilename();
-			System.out.println(fileRealName);
-			File saveFile = new File(filePath + File.separator + fileRealName);
-
-
-			String[] extension = fileRealName.split("\\.");
-			System.out.println(extension[extension.length-1]);
-			if(extension[extension.length-1].equals("exe") || extension[extension.length-1].equals("src") || extension[extension.length-1].equals("dll") || extension[extension.length-1].equals("ocx") || extension[extension.length-1].equals("cpl") || extension[extension.length-1].equals("drv") || extension[extension.length-1].equals("sys") || extension[extension.length-1].equals("vxd") || extension[extension.length-1].equals("obj")){
-				PEfile.add(saveFile);
-			}
-
-			try{
-				fileinputlist.get(i).transferTo(saveFile);
-			}catch (IllegalStateException e){
-				e.printStackTrace();
-			}catch (IOException e){
-				e.printStackTrace();
-			}
+			// 파일 이름 추출, ex) hello.exe
+			String fileName = fileinputlist.get(i).getOriginalFilename();
+			// PE파일 분류
+			checked_PEfile(fileName, upload_filePath, PEfileList, fileinputlist.get(i));
 
 		}
-		// commit test
-		info d = urlService.byteArrayToBinary(PEfile);
 
-		List<String> nList = new ArrayList<>();
-		List<String> mList = new ArrayList<>();
-		List<String> pList = new ArrayList<>();
-		List<String> uList = new ArrayList<>();
+		// PE파일들 분석
+		info d = urlService.byteArrayToBinary(PEfileList);
 
+		// receive_URL에 넘길 데이터 가져오기
+		List<String> nList = d.getFilenamelist();
+		List<String> mList = d.getMalware_result();
+		List<String> pList = d.getPacking_result();
+		List<String> uList = d.getUnpacking_result();
 
-		nList = d.getFilenamelist();
-		mList = d.getMalware_result();
-		pList = d.getPacking_result();
-		uList = d.getUnpacking_result();
-
+		// 클라이언트에게 보여줄 뷰(view) 생성
 		ModelAndView modelAndView = new ModelAndView("receive_URL");
 
+		// receive_URL에 데이터 넘기기
 		modelAndView.addObject("nList",nList);
 		modelAndView.addObject("pList",pList);
 		modelAndView.addObject("uList",uList);
